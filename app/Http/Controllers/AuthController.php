ea278768daafa5a5aaecfdfc916ea1bc6b43bcf6
Azuki.I <azuking69@hotmail.co.jp>
2026-03-26 23:36:20 +0900

    26.03.26 inserted login of AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,7 +12,8 @@ use Illuminate\Support\Facades\Hash;
 class AuthController extends Controller{
 
     // 新規会員
-    public function register(Request $request){
+    public function register(Request $request)
+    {
         // 入力チェック（バリデーション）
         $request->validate([
             'name' => 'required',
@@ -31,6 +32,33 @@ class AuthController extends Controller{
         return response()->json([
             'message' => 'register ok',
             'data' => $user,
+        ]);
+    }
+
+
+    // ログイン
+    public function login(Request $request)
+    {
+        // 入力チェック
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+        ]);
+
+        // メールアドレスでアカウントを探す
+        $user = User::where('email', $request->email)->first();
+
+        // 送られてきたパスワードとDBを照会
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            // ログイン失敗
+            return response()->json([
+                'message' => 'login fail'
+            ], 401);
+        }
+
+        // ログイン成功
+        return response()->json([
+            'message' => 'login ok'
         ]);
     }
 }
