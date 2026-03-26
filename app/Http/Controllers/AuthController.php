40452689fd807223c61f41a718d7be74eaad3f48
Azuki.I <azuking69@hotmail.co.jp>
2026-03-26 23:17:45 +0900

    26.03.26 inserted validate of AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,6 +12,13 @@ class AuthController extends Controller{
 
     // 新規会員
     public function register(Request $request){
+        // 入力チェック（バリデーション）
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6',
+        ]);
+
         // userテーブルに新規ユーザー登録
         $user = User::create([
                     'name' => $request->name,
