85cd6e45214f3ea2da61276682a9de7c6fb48f69
Azuki.I <azuking69@hotmail.co.jp>
2026-03-28 22:34:41 +0900

    26.03.28 inserted middleware of app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        // jwt.authという名前でmiddleware使えるようにする
+         $middleware->alias([
+            'jwt.auth' => \App\Http\Middleware\JwtAuthMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
