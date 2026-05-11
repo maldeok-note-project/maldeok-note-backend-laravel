bb03fdf489cdaf572a06f387b84d62538c19114d
Azuki.I <azuking69@hotmail.co.jp>
2026-05-11 18:09:43 +0900

    26.05.11 inserted logout of AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -87,7 +87,7 @@ class AuthController extends Controller{
     // ログアウト
     public function logout(Request $request)
     {
-        // トークン確認
+        // AuthorizationヘッダーからBearer Token取得
         $token = $request->bearerToken();
         // トークンが見つからないとき
         if (!$token) {
@@ -96,13 +96,18 @@ class AuthController extends Controller{
             ], 401);
         }
 
+        // JWTをdecodeしてpayload取得
         $decoded = JWT::decode($token, new Key(env('TOKEN_SECRET'), 'HS256'));
 
+        // revoked_tokens テーブルに保存
+        // → 今後このtokenは使用不可になる
         RevokedToken::create([
             'token' => $token,
+            // JWTのexp(有効期限)をDB保存用日時へ変換
             'expires_at' => date('Y-m-d H:i:s', $decoded->exp),
         ]);
 
+        // ログアウト成功表示
         return response()->json([
             'message' => 'logout ok'
         ], 200);
