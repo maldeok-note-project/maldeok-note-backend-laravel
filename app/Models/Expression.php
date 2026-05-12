1a52210ff14c1495a259ff59e63544134923838b
Azuki.I <azuking69@hotmail.co.jp>
2026-05-12 20:54:25 +0900

    26.05.12 inserted models of Expression.php

--- app/Models/Expression.php
+++ app/Models/Expression.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+// import
+use App\Models\User;
+use App\Models\SpeakerCategory;
 use Illuminate\Database\Eloquent\Model;
 
 class Expression extends Model
 {
-    //
+    // この表現を登録したユーザー
+    public function user()
+    {
+        // 「expressions.user_id → users.id」で自動連結
+        return $this->belongsTo(User::class);
+    }
+
+
+    // この表現が属する話者カテゴリ
+    public function speakerCategory()
+    {
+        // expressions.speaker_category_id → speaker_categories.id」で自動連結
+        return $this->belongsTo(SpeakerCategory::class);
+    }
 }
