95574ac8c99e5faa604e96e04af3a233a9a45b01
Azuki.I <azuking69@hotmail.co.jp>
2026-05-14 00:13:55 +0900

    26.05.13 made of SpeakerCategoryservice.php

---
+++ app/Services/SpeakerCategoryService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Services;
+
+// import
+use App\Models\SpeakerCategory;
+use App\Models\User;
+
+
+// ontrollerから呼び出され、DB操作や業務ルールの処理を行う
+class SpeakerCategoryService{
+    // 新しい話者カテゴリを作成
+    public function create(User $user, string $name): SpeakerCategory
+    {
+        // 重複確認
+        $exists = SpeakerCategory::where('user_id', $user->id)
+        ->where('name', $name)
+        ->exists();
+
+        // 重複があった場合エラー
+        if($exists){
+            throw new \DomainException('このカテゴリは登録されています。');
+        }
+
+        // カテゴリ新規作成
+        // User経由で作成することで、user_idが自動でセット
+        return $user->speakerCategories()->create([
+            'name' => $name,
+        ]);
+
+    }
+}
