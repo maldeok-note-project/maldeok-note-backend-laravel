d8cad06e235f1132a27b07df100ece64d631593d
Azuki.I <azuking69@hotmail.co.jp>
2026-05-21 22:18:27 +0900

    26.05.21 made ExpressionController.php

--- app/Http/Controllers/ExpressionController.php
+++ app/Http/Controllers/ExpressionController.php
@@ -2,10 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreExpressionRequest;
+use App\Services\ExpressionService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ExpressionController extends Controller
 {
+    // インスタンス保持
+    private ExpressionService $service;
+
+    // クラス作成時、自動呼出し
+    public function __construct(ExpressionService $service)
+    {
+        // Laravelが自動でServiceを挿入
+        $this->service = $service;
+    }
+
     /**
      * Display a listing of the resource.
      */
@@ -15,11 +28,30 @@ class ExpressionController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * 表現作成
      */
-    public function store(Request $request)
+    public function store(StoreExpressionRequest $request): JsonResponse
     {
-        //
+        // バリデーションデータ取得
+        $validated = $request->validated();
+
+        // JWT認証/ログイン中ユーザー
+        $user = $request->attributes->get('auth_user');
+
+        // Service経由で表現作成
+        try{
+            $expression = $this->service->create($user, $validated);
+        } catch (\DomainException $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+            ], 404);
+        }
+
+        // 成功レスポンス
+        return response()->json([
+            'message' => '表現を登録しました。',
+            'data' => $expression,
+        ], 201);
     }
 
     /**
