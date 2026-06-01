5c8fe66cbe28994fa374c65f400686d54924c02d
Azuki.I <azuking69@hotmail.co.jp>
2026-06-01 21:08:22 +0900

    refactor: 404エラーレスポンスを日本語に統一 (#20)
    * 26.05.23 inserted show of api.php
    
    * 26.05.23 inserted show of ExpressionService
    
    * 26.05.23 fixed code of ExpressionService
    
    * 26.05.23 fixed code of ExpressionService
    
    * 26.05.23 inserted show of ExpressionController.php
    
    * 26.06.01 inserted 404 response
    
    * 26.06.01 changed codes
    
    * 26.06.01 changed codes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,10 @@
 <?php
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,5 +20,12 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        // API->json
+        $exceptions->shouldRenderJsonWhen(fn() => true);
+        // findOrFailで見つからないとき(404)のレスポンスを日本語統一
+        $exceptions->render(function(\Symfony\Component\HttpKernel\Exception\NotFoundHttpException $e, Request $request){    
+            return response()->json([
+                'message' => 'リソースが見つかりませんでした。',
+            ], 404);
+        });
     })->create();
