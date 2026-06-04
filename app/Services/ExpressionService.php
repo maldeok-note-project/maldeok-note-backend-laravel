cdfe8c89503653811deffa2874a12bf86226e1cf
Azuki.I <azuking69@hotmail.co.jp>
2026-06-04 23:24:30 +0900

    26.06.04 inserted getAll codes of ExpressionService.php

--- app/Services/ExpressionService.php
+++ app/Services/ExpressionService.php
@@ -29,7 +29,7 @@ class ExpressionService
 
 
     // 表現一覧
-    public function getAll(User $user): LengthAwarePaginator
+    public function getAll(User $user, ?string $serach = null): LengthAwarePaginator
     {
         // 自分の表示だけ
         return $user->expressions()
